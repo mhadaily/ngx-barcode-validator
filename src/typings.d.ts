836e98f3c9767fe60501f41bce86481279249ac2
Majid <mhadaily@gmail.com>
2017-01-07 14:46:08 +0800

    Add: typing reference file

---
+++ src/typings.d.ts
@@ -0,0 +1,7 @@
+// Typings reference file, you can add your own global typings here
+// https://www.typescriptlang.org/docs/handbook/writing-declaration-files.html
+
+// declare var Quagga: any;
+// declare var System: any;
+// declare var module: { id: string };
+// declare var require: any;

ca362a98f33271be4d11c183dfa0113bbd27eacf
Majid <mhadaily@gmail.com>
2017-03-04 18:13:48 +0800

    update to angular-cli rc1 and 2.4.9

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -12,19 +12,19 @@ describe('AppComponent', () => {
     });
     TestBed.compileComponents();
   });
-
+  
   it('should create the app', async(() => {
     let fixture = TestBed.createComponent(AppComponent);
     let app = fixture.debugElement.componentInstance;
     expect(app).toBeTruthy();
   }));
-
+  
   it(`should have as title 'app works!'`, async(() => {
     let fixture = TestBed.createComponent(AppComponent);
     let app = fixture.debugElement.componentInstance;
     expect(app.title).toEqual('app works!');
   }));
-
+  
   it('should render title in a h1 tag', async(() => {
     let fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();

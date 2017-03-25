c96e4d1a6013d8398eeea71ccc92833e44feaed8
Majid <mhadaily@gmail.com>
2017-03-25 11:34:48 +0800

    format code

--- src/app/barcode/barcode.component.ts
+++ src/app/barcode/barcode.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-barcode',
@@ -6,5 +6,4 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./barcode.component.scss']
 })
 export class BarcodeComponent {
-  
 }
